361db4315748ba28425731c7065d7e0a0e7237a5
Meggie <meggiecheng01@gmail.com>
2022-10-18 22:29:41 -0400

    modified dates.java

--- lab2/dates.java
+++ lab2/dates.java
@@ -1,21 +1,53 @@
 import java.time.LocalDate;
-import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
 
 public class dates {
     public static void main(String[] args) {
+        //current date
         LocalDate now = LocalDate.now();
         System.out.println("current date: " + now);
+        //due dates
         LocalDate lab1 = LocalDate.of(2022, 9, 28);
-        Period diff1 = lab1.until(now);
-        System.out.println("period until due date: " + diff1);
         LocalDate lab2 = LocalDate.of(2022, 10, 26);
-        Period diff2 = lab2.until(now);
-        System.out.println("period until due date: " + diff2);
         LocalDate lab3 = LocalDate.of(2022,11, 23);
-        Period diff3 = lab3.until(now);
-        System.out.println("period until due date: " + diff3);
         LocalDate lab4 = LocalDate.of(2022, 12, 7);
-        Period diff4 = lab4.until(now);
-        System.out.println("period until due date: " + diff4);
+        /*
+        //allow user to input dates
+        Scanner input = new Scanner(System.in);
+        //boolean date = input.hasNext("MMDDYYYY");
+        System.out.println("input date(MMDDYYYY): ");
+        while(input.hasNextLong()) {
+            //System.out.println("days until due date: " + daysBetweenDates(now, date) + " days");
+        }
+        input.close();
+         */
+        //display
+        display(daysBetweenDates(now, lab1));
+        display(daysBetweenDates(now, lab2));
+        display(daysBetweenDates(now, lab3));
+        display(daysBetweenDates(now, lab4));
+    }
+    /**
+     * method calculates number of days between 2 dates
+     * @param now -- current date
+     * @param date -- date of comparison
+     * @return - number of days between dates
+     */
+    public static long daysBetweenDates(LocalDate now, LocalDate date) {
+        long days = ChronoUnit.DAYS.between(now, date);
+        return days;
+    }
+    /**
+     * displays output
+     * @param days until due date
+     */
+    public static void display (long days) {
+        //nagative days between dates
+        if(days < 0) {
+            System.out.println("past due: " + days*-1 + " days");
+        } else {
+            System.out.println("days until due date: " + days + " days");
+        }
     }
 }
